c1ef85692542734aef9587469f1db1e6d75da5fd
Yass32 <yasserzoaka@gmail.com>
2025-06-23 20:42:20 +0300

    hint generator

--- backend/services/openrouterService.js
+++ backend/services/openrouterService.js
@@ -45,7 +45,7 @@ export const getAIRecommendation = async (userAnswers) => {
         const response = await axios.post(
             process.env.API_URL, // Use the API URL from environment variables
             {  
-                model: "deepseek/deepseek-r1-0528:free", // AI model to use
+                model: "mistralai/mistral-small-3.2-24b-instruct:free", // AI model to use
                 messages: [
                     { role: "system", content: "You are an AI assistant that provides concise and well-structured programming recommendations."}, // System role description
                     { role: "user", content: prompt } // User's request to the AI
